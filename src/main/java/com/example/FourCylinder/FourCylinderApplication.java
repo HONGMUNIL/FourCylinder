eda194b6b9265f03ea43591ddfd4878f37257f9f
MunIL HONG <bear4564@naver.com>
2025-04-28 22:36:03 +0900

    패키지생성

---
+++ src/main/java/com/example/FourCylinder/FourCylinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.FourCylinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourCylinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourCylinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FourCylinder/FourCylinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FourCylinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourCylinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
